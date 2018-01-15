bb2af09feb494b1969e481d2b3749bf5d671e88e
Loborg <loborger@gmail.com>
2018-01-15 23:30:17 +0100

    Add activity_evaluating.xml Add design xml code to evaluation xml Add application logic to MainActivity Add some more dimension property Add some more string property

--- app/src/main/java/com/example/android/personalincometaxquiz/MainActivity.java
+++ app/src/main/java/com/example/android/personalincometaxquiz/MainActivity.java
@@ -1,20 +1,81 @@
 package com.example.android.personalincometaxquiz;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameTextField;
+    private EditText emailTextField;
+    private ImageView nextIconImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextIconImage = findViewById(R.id.next_image_01);
+        nameTextField = findViewById(R.id.name_textfield);
+        emailTextField = findViewById(R.id.email_textfield);
+
+        nameTextField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!emailTextFieldText().equals("")){
+                    nextIconImage.setColorFilter(Color.BLACK);
+                } else {
+                    nextIconImage.setColorFilter(Color.LTGRAY);
+                }
+            }
+        });
+
+        emailTextField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!nameTextFieldText().equals("")){
+                    nextIconImage.setColorFilter(Color.BLACK);
+                } else {
+                    nextIconImage.setColorFilter(Color.LTGRAY);
+                }
+            }
+        });
     }
 
-    public void nextPage(View v){
-        Intent openQuizOne = new Intent(MainActivity.this, Evaluation.class);
-        startActivity(openQuizOne);
+    public String nameTextFieldText(){
+        return nameTextField.getText().toString();
+    }
+
+    public String emailTextFieldText(){
+        return emailTextField.getText().toString();
+    }
+
+    public void nextIconClick(View v){
+        if (!nameTextFieldText().equals("") && !emailTextFieldText().equals("")){
+            Intent openFirstQuiz = new Intent(this, QuizOne.class);
+            startActivity(openFirstQuiz);
+        } else {
+            Toast emptiTextFieldWarning = Toast.makeText(this, "You didn’t fill out all the required fields!", Toast.LENGTH_SHORT);
+            emptiTextFieldWarning.show();
+        }
     }
 }
